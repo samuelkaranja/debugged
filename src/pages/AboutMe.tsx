f595e7e41faabbeb6ce70793c92919d9cfcd0d6f
samuelkaranja <samuelkaranja007@gmail.com>
2025-09-29 23:38:35 +0300

    Redesigned the AboutMe page

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -6,11 +6,6 @@ import Sam from "../assets/Sam.jpeg";
 const AboutMe: React.FC = () => {
   return (
     <div className="h-full flex flex-col md:flex-row gap-10 justify-center items-center md:items-start pt-10 px-5">
-      {/* Profile Section */}
-      <div className="w-full md:w-1/2">
-        <Profile />
-      </div>
-
       {/* Image Section */}
       <div className="hidden md:flex justify-center items-center md:w-85 md:h-85">
         <img
@@ -18,6 +13,11 @@ const AboutMe: React.FC = () => {
           alt="Coding illustration"
           className="w-full h-full object-cover rounded-lg shadow-lg"
         />
+      </div>
+
+      {/* Profile Section */}
+      <div className="w-full md:w-1/2">
+        <Profile />
       </div>
     </div>
   );
