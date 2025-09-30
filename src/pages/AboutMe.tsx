07ff3aecd5a051866ad80b6e3f83402e377352ae
samuelkaranja <samuelkaranja007@gmail.com>
2025-09-30 20:19:28 +0300

    Resized the image in the AboutMe page and added a style to the heading in the Profile component

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -7,7 +7,7 @@ const AboutMe: React.FC = () => {
   return (
     <div className="h-full flex flex-col md:flex-row gap-10 justify-center items-center md:items-start pt-10 px-5">
       {/* Image Section */}
-      <div className="hidden md:flex justify-center items-center md:w-85 md:h-85">
+      <div className="hidden md:flex justify-center items-center md:w-55 md:h-55">
         <img
           src={Sam}
           alt="Coding illustration"
